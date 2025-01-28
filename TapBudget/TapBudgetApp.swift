dce0cff275507f66213d8bed9a3cb6b7899a7b9d
Saichand Akkineni <cmob.automation@gmail.com>
2025-01-28 01:10:59 -0500

    Initial Commit

---
+++ TapBudget/TapBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapBudgetApp.swift
+//  TapBudget
+//
+//  Created by SAICHAND AKKINENI on 2025-01-28.
+//
+
+import SwiftUI
+
+@main
+struct TapBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TapBudgetTests/TapBudgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  TapBudgetTests.swift
+//  TapBudgetTests
+//
+//  Created by SAICHAND AKKINENI on 2025-01-28.
+//
+
+import Testing
+@testable import TapBudget
+
+struct TapBudgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
